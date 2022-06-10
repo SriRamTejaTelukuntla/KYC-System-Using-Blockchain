a0b4115322de12eccd53b402856b39ff4994274b
SRT <sriramteja2000@gmail.com>
2022-06-10 19:33:26 +0530

    redesigned homepage

--- app/src/index.js
+++ app/src/index.js
@@ -360,3 +360,38 @@ window.addEventListener("load", function() {
 
   App.start();
 });
+
+
+
+//Homepage code
+
+(function() {
+    
+    /*=====================================
+    Sticky
+    ======================================= */
+    window.onscroll = function () {
+        var header_navbar = document.querySelector(".navbar-area");
+        var sticky = header_navbar.offsetTop;
+        var logo = document.querySelector(".navbar-brand img");
+
+        if (window.pageYOffset > sticky) {
+            header_navbar.classList.add("sticky");
+            logo.src = '../images/logo/KYC_Screen-Logo_White.png'
+        } else {
+            header_navbar.classList.remove("sticky");
+            logo.src = '../images/logo/KYC_Screen-Logo_White.webp'
+        }
+
+
+
+        // show or hide the back-top-top button
+        var backToTo = document.querySelector(".scroll-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "flex";
+        } else {
+            backToTo.style.display = "none";
+        }
+    };
+
+})();
