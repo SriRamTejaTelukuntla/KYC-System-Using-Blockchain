bd70ea246019f3bac248ee840b127a20c15a1d0e
SRT <sriramteja2000@gmail.com>
2022-06-12 21:09:19 +0530

    Upgraded code to support banks & demat

--- contracts/KycContract.sol
+++ contracts/KycContract.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.10;
  
 contract KycContract {
- 
-    struct registeredUserDetails {
+
+    // Structure of Bank
+    struct registeredBankUserDetails {
         string fullname;
         string fathername;
         string aadhar_number;
@@ -13,7 +14,7 @@ contract KycContract {
         string dob;
     }
 
-    struct verifiedUserDetails {
+    struct verifiedBankUserDetails {
         string fullname;
         string fathername;
         string aadhar_number;
@@ -23,110 +24,234 @@ contract KycContract {
         string dob;
     }
 
-    // Mappping
-    mapping(string =>registeredUserDetails) registeredUserMap;
-    mapping(string =>verifiedUserDetails) verifiedUserMap;
+    // Structure of Demat
+    struct registeredDematUserDetails {
+        string fullname;
+        string fathername;
+        string pan_number;
+        uint phone_number;
+        string dob;
+    }
 
-    // Initializing array
-    string[] registeredUsersArray;
-    string[] verifiedUsersArray;
+    struct verifiedDematUserDetails {
+        string fullname;
+        string fathername;
+        string pan_number;
+        uint phone_number;
+        string dob;
+    }
 
-    // set Function for setUser   
-    function setRegisteredUser (string memory _fullname, string memory _fathername, string memory _aadhar_number,
+    // Mappping of Bank
+    mapping(string =>registeredBankUserDetails) registeredBankUserMap;
+    mapping(string =>verifiedBankUserDetails) verifiedBankUserMap;
+
+    // Initializing array of Bank
+    string[] registeredBankUsersArray;
+    string[] verifiedBankUsersArray;
+
+    // Mappping of Demat
+    mapping(string =>registeredDematUserDetails) registeredDematUserMap;
+    mapping(string =>verifiedDematUserDetails) verifiedDematUserMap;
+
+    // Initializing array of Demat
+    string[] registeredDematUsersArray;
+    string[] verifiedDematUsersArray;
+
+    // set Function for BankUser   
+    function setRegisteredBankUser (string memory _fullname, string memory _fathername, string memory _aadhar_number,
                                 uint _phone_number, string memory _useraddress, string memory _gender, string memory _dob)
     public {
-        require(keccak256(abi.encodePacked((registeredUserMap[_aadhar_number].aadhar_number)))
+        require(keccak256(abi.encodePacked((registeredBankUserMap[_aadhar_number].aadhar_number)))
                         != keccak256(abi.encodePacked(_aadhar_number)));
 
-        registeredUserMap[_aadhar_number].fullname = _fullname ;
-        registeredUserMap[_aadhar_number].fathername = _fathername ;
-        registeredUserMap[_aadhar_number].aadhar_number = _aadhar_number ;
-        registeredUserMap[_aadhar_number].phone_number = _phone_number ;
-        registeredUserMap[_aadhar_number].useraddress = _useraddress ;
-        registeredUserMap[_aadhar_number].gender = _gender ;
-        registeredUserMap[_aadhar_number].dob = _dob ;
+        registeredBankUserMap[_aadhar_number].fullname = _fullname ;
+        registeredBankUserMap[_aadhar_number].fathername = _fathername ;
+        registeredBankUserMap[_aadhar_number].aadhar_number = _aadhar_number ;
+        registeredBankUserMap[_aadhar_number].phone_number = _phone_number ;
+        registeredBankUserMap[_aadhar_number].useraddress = _useraddress ;
+        registeredBankUserMap[_aadhar_number].gender = _gender ;
+        registeredBankUserMap[_aadhar_number].dob = _dob ;
  
-        registeredUsersArray.push(_aadhar_number) ;
+        registeredBankUsersArray.push(_aadhar_number) ;
     }
  
-    function setVerifiedUser (string memory _fullname, string memory _fathername, string memory _aadhar_number,
+    function setVerifiedBankUser (string memory _fullname, string memory _fathername, string memory _aadhar_number,
                               uint _phone_number, string memory _useraddress, string memory _gender, string memory _dob)
     public {
-        require(keccak256(abi.encodePacked((verifiedUserMap[_aadhar_number].aadhar_number)))
+        require(keccak256(abi.encodePacked((verifiedBankUserMap[_aadhar_number].aadhar_number)))
             != keccak256(abi.encodePacked(_aadhar_number)));
 
-        verifiedUserMap[_aadhar_number].fullname = _fullname ;
-        verifiedUserMap[_aadhar_number].fathername = _fathername ;
-        verifiedUserMap[_aadhar_number].aadhar_number = _aadhar_number ;
-        verifiedUserMap[_aadhar_number].phone_number = _phone_number ;
-        verifiedUserMap[_aadhar_number].useraddress = _useraddress ;
-        verifiedUserMap[_aadhar_number].gender = _gender ;
-        verifiedUserMap[_aadhar_number].dob = _dob ;
+        verifiedBankUserMap[_aadhar_number].fullname = _fullname ;
+        verifiedBankUserMap[_aadhar_number].fathername = _fathername ;
+        verifiedBankUserMap[_aadhar_number].aadhar_number = _aadhar_number ;
+        verifiedBankUserMap[_aadhar_number].phone_number = _phone_number ;
+        verifiedBankUserMap[_aadhar_number].useraddress = _useraddress ;
+        verifiedBankUserMap[_aadhar_number].gender = _gender ;
+        verifiedBankUserMap[_aadhar_number].dob = _dob ;
  
-        verifiedUsersArray.push(_aadhar_number) ;
+        verifiedBankUsersArray.push(_aadhar_number) ;
     }
 
-    function getRegisteredUser (string memory _aadhar_number) view public
+         // set Function for DematUser   
+    function setRegisteredDematUser (string memory _fullname, string memory _fathername, string memory  _pan_number,
+                                uint _phone_number, string memory _dob)
+    public {
+        require(keccak256(abi.encodePacked((registeredDematUserMap[_pan_number].pan_number)))
+                        != keccak256(abi.encodePacked(_pan_number)));
+
+        registeredDematUserMap[_pan_number].fullname = _fullname ;
+        registeredDematUserMap[_pan_number].fathername = _fathername ;
+        registeredDematUserMap[_pan_number].pan_number =  _pan_number ;
+        registeredDematUserMap[_pan_number].phone_number = _phone_number ;
+        registeredDematUserMap[_pan_number].dob = _dob ;
+ 
+        registeredDematUsersArray.push(_pan_number) ;
+    }
+
+     function setVerifiedDematUser (string memory _fullname, string memory _fathername, string memory  _pan_number,
+                                uint _phone_number, string memory _dob)
+    public {
+        require(keccak256(abi.encodePacked((verifiedDematUserMap[_pan_number].pan_number)))
+                        != keccak256(abi.encodePacked(_pan_number)));
+
+        verifiedDematUserMap[_pan_number].fullname = _fullname ;
+        verifiedDematUserMap[_pan_number].fathername = _fathername ;
+        verifiedDematUserMap[_pan_number].pan_number =  _pan_number ;
+        verifiedDematUserMap[_pan_number].phone_number = _phone_number ;
+        verifiedDematUserMap[_pan_number].dob = _dob ;
+ 
+        verifiedDematUsersArray.push(_pan_number) ;
+    }
+
+    // get Function for BankUser   
+    function getRegisteredBankUser (string memory _aadhar_number) view public
         returns(string memory ,string memory, string memory, uint, string memory, string memory, string memory) {
         return
         (
-            registeredUserMap[_aadhar_number].fullname,
-            registeredUserMap[_aadhar_number].fathername,
-            registeredUserMap[_aadhar_number].aadhar_number,
-            registeredUserMap[_aadhar_number].phone_number,
-            registeredUserMap[_aadhar_number].useraddress,
-            registeredUserMap[_aadhar_number].gender,
-            registeredUserMap[_aadhar_number].dob
+            registeredBankUserMap[_aadhar_number].fullname,
+            registeredBankUserMap[_aadhar_number].fathername,
+            registeredBankUserMap[_aadhar_number].aadhar_number,
+            registeredBankUserMap[_aadhar_number].phone_number,
+            registeredBankUserMap[_aadhar_number].useraddress,
+            registeredBankUserMap[_aadhar_number].gender,
+            registeredBankUserMap[_aadhar_number].dob
         );
     }
  
-    function getVerifiedUser (string memory _aadhar_number) view public
+    function getVerifiedBankUser (string memory _aadhar_number) view public
         returns(string memory ,string memory, string memory, uint, string memory, string memory, string memory) {
         return
         (
-            verifiedUserMap[_aadhar_number].fullname,
-            verifiedUserMap[_aadhar_number].fathername,
-            verifiedUserMap[_aadhar_number].aadhar_number,
-            verifiedUserMap[_aadhar_number].phone_number,
-            verifiedUserMap[_aadhar_number].useraddress,
-            verifiedUserMap[_aadhar_number].gender,
-            verifiedUserMap[_aadhar_number].dob
+            verifiedBankUserMap[_aadhar_number].fullname,
+            verifiedBankUserMap[_aadhar_number].fathername,
+            verifiedBankUserMap[_aadhar_number].aadhar_number,
+            verifiedBankUserMap[_aadhar_number].phone_number,
+            verifiedBankUserMap[_aadhar_number].useraddress,
+            verifiedBankUserMap[_aadhar_number].gender,
+            verifiedBankUserMap[_aadhar_number].dob
         );
     }
 
-    function checkVerifiedUser (string memory _aadhar_number) view public returns(bool) {
-        if (keccak256(abi.encodePacked((verifiedUserMap[_aadhar_number].aadhar_number)))
+    // get Function for DematUser  
+    function getRegisteredDematUser (string memory _pan_number) view public
+        returns(string memory ,string memory, string memory, uint, string memory) {
+        return
+        (
+            registeredDematUserMap[_pan_number].fullname,
+            registeredDematUserMap[_pan_number].fathername,
+            registeredDematUserMap[_pan_number].pan_number,
+            registeredDematUserMap[_pan_number].phone_number,
+            registeredDematUserMap[_pan_number].dob
+        );
+    }
+
+    function getVerifiedDematUser (string memory _pan_number) view public
+        returns(string memory ,string memory, string memory, uint, string memory) {
+        return
+        (
+            verifiedDematUserMap[_pan_number].fullname,
+            verifiedDematUserMap[_pan_number].fathername,
+            verifiedDematUserMap[_pan_number].pan_number,
+            verifiedDematUserMap[_pan_number].phone_number,
+            verifiedDematUserMap[_pan_number].dob
+        );
+    }
+
+    // check Function for BankUser  
+    function checkVerifiedBankUser (string memory _aadhar_number) view public returns(bool) {
+        if (keccak256(abi.encodePacked((verifiedBankUserMap[_aadhar_number].aadhar_number)))
             == keccak256(abi.encodePacked(_aadhar_number)))
             return true;
         else
             return false;
     }
     
-    function checkRegisteredUser (string memory _aadhar_number) view public returns(bool) {
-        if (keccak256(abi.encodePacked((registeredUserMap[_aadhar_number].aadhar_number))) 
+    function checkRegisteredBankUser (string memory _aadhar_number) view public returns(bool) {
+        if (keccak256(abi.encodePacked((registeredBankUserMap[_aadhar_number].aadhar_number))) 
             == keccak256(abi.encodePacked(_aadhar_number)))
             return true;
         else
             return false;
     }
 
-    function getCountOfRegisteredUsers() view public returns (uint)
-    {
-        return registeredUsersArray.length;
+    // check Function for DematUser  
+    function checkVerifiedDematUser (string memory _pan_number) view public returns(bool) {
+        if (keccak256(abi.encodePacked((verifiedDematUserMap[ _pan_number].pan_number)))
+            == keccak256(abi.encodePacked( _pan_number)))
+            return true;
+        else
+            return false;
+    }
+    
+    function checkRegisteredDematUser (string memory _pan_number) view public returns(bool) {
+        if (keccak256(abi.encodePacked((registeredDematUserMap[ _pan_number].pan_number))) 
+            == keccak256(abi.encodePacked( _pan_number)))
+            return true;
+        else
+            return false;
     }
 
-    function getCountOfVerifiedUsers() view public returns (uint)
+    // count Function for BankUser  
+    function getCountOfRegisteredBankUsers() view public returns (uint)
     {
-        return verifiedUsersArray.length;
+        return registeredBankUsersArray.length;
     }
 
-    function getAllRegisteredIDs() view public returns(string[] memory)
+    function getCountOfVerifiedBankUsers() view public returns (uint)
     {
-            return registeredUsersArray;
+        return verifiedBankUsersArray.length;
     }
 
-    function getAllVerifiedIDs() view public returns(string[] memory)
+    // count Function for DematUser  
+    function getCountOfRegisteredDematUsers() view public returns (uint)
     {
-            return verifiedUsersArray;
+        return registeredDematUsersArray.length;
+    }
+
+    function getCountOfVerifiedDematUsers() view public returns (uint)
+    {
+        return verifiedDematUsersArray.length;
+    }
+
+    // get Function for BankUserIDs  
+    function getAllRegisteredBankUsersIDs() view public returns(string[] memory)
+    {
+        return registeredBankUsersArray;
+    }
+
+    function getAllVerifiedBankUsersIDs() view public returns(string[] memory)
+    {
+        return verifiedBankUsersArray;
+    }
+
+    // get Function for DematUserIDs  
+     function getAllRegisteredDematUsersIDs() view public returns(string[] memory)
+    {
+        return registeredDematUsersArray;
+    }
+
+    function getAllVerifiedDematUsersIDs() view public returns(string[] memory)
+    {
+        return verifiedDematUsersArray;
     }
 }
