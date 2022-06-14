e0bd9e2cf62560fb946ab601c77d441fd2b372c1
SRT <sriramteja2000@gmail.com>
2022-06-14 20:12:28 +0530

    Updated file name

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   plugins: [
 
     new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
-    new CopyWebpackPlugin([{ from: "./src/html/kyc.html", to: "html/kyc.html" }]),
+    new CopyWebpackPlugin([{ from: "./src/html/bank-kyc.html", to: "html/bank-kyc.html" }]),
     new CopyWebpackPlugin([{ from: "./src/html/registered-users.html", to: "html/registered-users.html" }]),
     new CopyWebpackPlugin([{ from: "./src/html/pending-users.html", to: "html/pending-users.html" }]),
     new CopyWebpackPlugin([{ from: "./src/html/verified-users.html", to: "html/verified-users.html" }]),
